6146e2b2427cafd4e145bf38b80a8c184c2dace9
liamattard <liam.dratta@gmail.com>
2019-05-16 10:46:28 +0200

    menu update

--- views/movement.js
+++ views/movement.js
@@ -7,8 +7,8 @@ document.addEventListener("mousemove",function(event){
     var y = event.clientY;
 
 
-        elem.style.left =  x/10 + "px";
-        elem.style.top =  y/10 + "px";
+        elem.style.left =  x/30 + "px";
+        elem.style.top =  y/30 + "px";
         elem.style.fontSize = x/40 +"px";
 
 });
