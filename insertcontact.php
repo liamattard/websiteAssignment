e4c1dce59ea4e85cec5741c0aa0835f1e60ef9a3
corinnegatt <gatt.corinne@gmail.com>
2019-05-28 19:52:06 +0200

    validation trial

--- insertcontact.php
+++ insertcontact.php
@@ -12,7 +12,25 @@
                     echo "Failed to connect to MySql: ". mysqli_connect_error();
                 }
     
-                          
+                $nameErr = $surnameErr = $emailErr = $messageErr ="";
+                $name = $email = $message = "";
+
+                
+                    if (!empty($_POST["name"])) {
+                        $name = clean_input($_POST["name"]);
+                        
+                        if(strlen($name)>50){
+                            $nameErr=  "Messages cannot be longer than 50 characters.";
+                        }
+                    }
+                    else
+                    {
+                        $nameErr = "Name is required";
+                    }
+
+
+
+                
                           $sql2 = "INSERT INTO contact (name, surname, email, subject, message) 
                            VALUES
                            ('$_POST[name]','$_POST[surname]','$_POST[email]','$_POST[subject]', '$_POST[message]' )";
@@ -29,5 +47,14 @@
 
                            mysqli_close($connect);
 
-                           header('Location: contacttrail.php');                
-         ?>
+                           header('Location: contacttrail.php');    
+                           
+                           function clean_input($data) {
+                            $data = trim($data);
+                            $data = stripslashes($data);
+                            $data = htmlspecialchars($data);
+                            return $data;
+                        }
+
+                       
+          
