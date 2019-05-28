017f124a00396c8dde5304c72efe0665eabf12cd
liamattard <liam.dratta@gmail.com>
2019-05-28 22:49:23 +0200

    fixing error messages

--- contacttrail.php
+++ contacttrail.php
@@ -15,14 +15,17 @@
     $twig->setLexer($lexer);
     
     $loggedIn = false;
-  
+    $wrongDetails = false;
 
+    if (isset($_SESSION["wrongdetails"])) {
+        $wrongDetails = true;
+    }
     if (isset($_SESSION["name"])) {
         $loggedIn = true;
-        echo $twig->render('contactUs.html', ['loggedIn' => $loggedIn, 'user' => $_SESSION["name"], 'page' => true]);
+        echo $twig->render('contactUs.html', ['loggedIn' => $loggedIn, 'user' => $_SESSION["name"], 'page' => true, 'error' => $wrongDetails, 'message' => $_SESSION["wrongdetails"]]);
     
     } else {
-        echo $twig->render('contactUs.html', ['loggedIn' => $loggedIn,'page' => false]);
+        echo $twig->render('contactUs.html', ['loggedIn' => $loggedIn,'page' => false,'error' => $wrongDetails, 'message' => $_SESSION["wrongdetails"]]);
     
     }
 
--- insertcontact.php
+++ insertcontact.php
@@ -1,128 +1,99 @@
 
 <?php
-                 
 
-                 include_once 'contactphp.php';
 
-                $d = new Db();
-                $connect = $d->connect();
+include_once 'contactphp.php';
+session_start();
+$d = new Db();
+$connect = $d->connect();
 
-                if(mysqli_connect_errno())
-                {
+if (mysqli_connect_errno()) {
 
-                    echo "Failed to connect to MySql: ". mysqli_connect_error();
-                }
+    echo "Failed to connect to MySql: " . mysqli_connect_error();
+}
+
+
+
+if (!empty($_POST["name"])) {
+        if(strlen($_POST["name"]) > 50){
+
+        $_SESSION["wrongdetails"] = "not more than 50 characters";
+        header('Location: ./contacttrail.php');
+
+        }
+} else {
+    $_SESSION["wrongdetails"] = "no name filled";
+    header('Location: contacttrail.php');
+}
+
+if (!empty($_POST["surname"]) ) {
+
+    if(strlen($_POST["surname"]) > 50){
+
+        $_SESSION["wrongdetails"] = "too long surname";
+        header('Location: contacttrail.php');
+    }
     
-                $_SESSION["wrongdetails"] = false;
-                $nameErr = $surnameErr = $emailErr = $messageErr ="";
-                $name =$surname= $email = $message = "";
+} else {
+    $_SESSION["wrongdetails"] = "no surname";
+    header('Location: contacttrail.php');
+}
 
+if (!empty($_POST["email"])) {
 
-                
-                    if (!empty($_POST["name"])) {
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
 
-                        if(strlen($name)>50){
-
-                            $_SESSION["wrongdetails"] = true;
-                            header('Location: contacttrail.php');
-                            
-                        }
-                    }
-                    else
-                    {
-                        $_SESSION["wrongdetails"] = true;
-                        header('Location: contacttrail.php');
-                        
-                    }
-
-                    if (!empty($_POST["surname"])) {
-
-                        if(strlen($surname)>50){
-
-                            $_SESSION["wrongdetails"] = true;
-                            header('Location: contacttrail.php');
-                            
-                        }
-                    }
-                    else
-                    {
-                        $_SESSION["wrongdetails"] = true;
-                        header('Location: contacttrail.php');
-                       
-                    }
-
-                    if (!empty($_POST["email"])) {
-                        
-                        if (!filter_var($email, FILTER_VALIDATE_EMAIL))
-                        {
-                            $emailErr= 'You did not enter a valid email.';
-                            $_SESSION["wrongdetails"] = true;
-                            header('Location: contacttrail.php');
-                            
-                        }
-                    }
-                    else
-                    {
-                        $emailErr = "Email is required";
-                        $_SESSION["wrongdetails"] = true;
-                        header('Location: contacttrail.php');
-                        
-                    }
+            $_SESSION["wrongdetails"] = "invalid email";
+            header('Location: ./contacttrail.php');
         
-                    if (!empty($_POST["message"])) {
-                        $message = clean_input($_POST["message"]);
-                        if(strlen($message)>50){
-                            $messageErr=  "Messages cannot be longer than 50 characters.";
-                            $_SESSION["wrongdetails"] = true;
-                            header('Location: contacttrail.php');
-                            
-                        }
-                    }
-                    else
-                    {
-                        $messageErr = "Message is required";
-                        $_SESSION["wrongdetails"] = true;
-                        header('Location: contacttrail.php');
-                        
+        }       
+} else {
+    $_SESSION["wrongdetails"] = "no email filled";
+    header('Location: contacttrail.php');
+}
+
+if (!empty($_POST["message"])) {
+    $message = clean_input($_POST["message"]);
+    if (strlen($message) > 50) {
+     
+        $_SESSION["wrongdetails"] = "message longer than 50 character";
+        header('Location: contacttrail.php');
+    }
+} else {
+  
+
+    $_SESSION["wrongdetails"] = "empty message";
+    header('Location: contacttrail.php');
+}
 
 
-                    }
-                       
-               
 
 
-                    if($_SESSION["wrongdetails"] == false)
-                    {
-                        
-                        $sql2 = "INSERT INTO contact (name, surname, email, subject, message) 
+if (!isset($_SESSION["wrongdetails"])) {
+
+    $sql2 = "INSERT INTO contact (name, surname, email, subject, message) 
                         VALUES
                         ('$_POST[name]','$_POST[surname]','$_POST[email]','$_POST[subject]', '$_POST[message]' )";
 
-                        if(!mysqli_query($connect, $sql2))
-                        {
+    if (!mysqli_query($connect, $sql2)) {
 
-                         die('Error: ' . mysqli_error($connect));
+        die('Error: ' . mysqli_error($connect));
+    }
+    echo "1 row added";
 
-                        }
-                         echo"1 row added";
+    mysqli_close($connect);
 
-                         mysqli_close($connect);
+    header('Location: contacttrail.php');
+}
 
-                         header('Location: contacttrail.php');    
-                           
 
-                    }
 
-                
-                           
 
-                          
-                           function clean_input($data) {
-                            $data = trim($data);
-                            $data = stripslashes($data);
-                            $data = htmlspecialchars($data);
-                            return $data;
-                        }
 
-                       
-          
+function clean_input($data)
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
