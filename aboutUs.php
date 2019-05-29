c42056d12d75269ed05dd7dccf943ea6f1678a0e
gabrieleborg26 <gabrieleborg26@gmail.com>
2019-05-29 07:11:49 +0200

    About us page

---
+++ aboutUs.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+require './vendor/autoload.php';
+
+
+$loader = new Twig_Loader_Filesystem('views');
+$twig = new Twig_Environment($loader);
+
+$lexer = new Twig_Lexer($twig, array(
+    'tag_block'      => array('{', '}'),
+    'tag_variable'   => array('{{', '}}')
+));
+
+$twig->setLexer($lexer);
+
+$loggedIn = false;
+
+
+    echo $twig->render('about.html');
+
+
+
+?>
